d7436b51529cb7eac9a5e63ac114a1a4bb601ca9
Thenecromance <1301151826@qq.com>
2024-06-28 18:47:19 +0800

    fixed the resource control's function

--- server/resourceControl/resourceControl.go
+++ server/resourceControl/resourceControl.go
@@ -11,9 +11,18 @@ type ResourceControl struct {
 
 func (rc *ResourceControl) Apply(engine *gin.Engine) {
 
-	engine.GET("/", func(c *gin.Context) {
+	if rc.cfg.HtmlFiles != nil && len(rc.cfg.HtmlFiles) > 0 {
+		//engine.LoadHTMLFiles(rc.cfg.HtmlFiles...)
+		if len(rc.cfg.HtmlFiles) == 1 {
+			engine.LoadHTMLGlob(rc.cfg.HtmlFiles[0])
+		} else {
+			engine.LoadHTMLFiles(rc.cfg.HtmlFiles...)
+		}
+	}
+
+	/*	engine.GET("/", func(c *gin.Context) {
 		c.File("dist/index.html")
-	})
+	})*/
 
 	if rc.cfg.NoMethod != "" {
 		engine.NoMethod(func(c *gin.Context) {
@@ -37,14 +46,10 @@ func (rc *ResourceControl) Apply(engine *gin.Engine) {
 	if rc.cfg.Redirects != nil && len(rc.cfg.Redirects) > 0 {
 		for redirect, target := range rc.cfg.Redirects {
 			log.Infof("Redirecting %s to %s", redirect, target)
-			engine.Any(redirect, func(c *gin.Context) {
-				c.Redirect(301, target)
+			engine.GET(redirect, func(c *gin.Context) {
+				c.HTML(200, "index.html", gin.H{})
 			})
 		}
-	}
-
-	if rc.cfg.HtmlFiles != nil && len(rc.cfg.HtmlFiles) > 0 {
-		engine.LoadHTMLFiles(rc.cfg.HtmlFiles...)
 	}
 
 }
